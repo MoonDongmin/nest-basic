3b4d242b6e736bf94726662c582b89169fa44028
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-05-01 11:09:26 +0900

    Table Inheritance

---
+++ typeorm-practice/src/entity/inheritance.entity.ts
@@ -0,0 +1,64 @@
+import {
+  ChildEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  TableInheritance,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class BaseModel {
+  // 엔티티 데코레이터가 없어서 테이블이 생성되지 않음
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
+@Entity()
+export class BookModel extends BaseModel {
+  @Column()
+  name: string;
+}
+
+@Entity()
+export class CarModel extends BaseModel {
+  @Column()
+  brand: string;
+}
+
+// 하나의 테이블로 2개의 테이블 관리할 떄 다음과 같이 함
+@Entity()
+@TableInheritance({
+  column: {
+    name: 'type',
+    type: 'varchar',
+  },
+})
+export class SingleBaseModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
+@ChildEntity()
+export class ComputerModel extends SingleBaseModel {
+  @Column()
+  brand: string;
+}
+
+@ChildEntity()
+export class AirplaneModel extends SingleBaseModel {
+  @Column()
+  country: string;
+}
