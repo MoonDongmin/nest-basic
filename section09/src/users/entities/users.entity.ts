652ff3d1a7133516075cb3bc8a1b84270d9841ef
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-05-09 15:58:20 +0900

    Enum Column 생성하기

--- section09/src/users/entities/users.entity.ts
+++ section09/src/users/entities/users.entity.ts
@@ -1,21 +1,33 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-enum RolesEnum {
-  USER = 'USER',
-  ADMIN = 'ADMIN',
-}
+import { RolesEnum } from '../const/roles.const';
 
 @Entity()
 export class UsersModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    // 1)
+    length: 20,
+    // 2)
+    unique: true,
+  })
+  // 1) 길이가 20을 넘지 않을 것
+  // 2) 유일무이한 값이 될 것
   nickname: string;
 
-  @Column()
+  @Column({
+    unique: true,
+  })
+  // 1) 유일무이한 값이 될 것
   email: string;
 
   @Column()
   password: string;
+
+  @Column({
+    enum: Object.values(RolesEnum),
+    default: RolesEnum.USER,
+  })
+  role: RolesEnum;
 }
