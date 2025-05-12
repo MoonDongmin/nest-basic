34324bba5dae1322421e774f49754be565637916
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-05-12 13:18:23 +0900

    registerWithEmail 함수 정의하기

--- section10-jwt/src/auth/const/auth.const.ts
+++ section10-jwt/src/auth/const/auth.const.ts
@@ -1,1 +1,2 @@
 export const JWT_SECRET = 'dongmin';
+export const HASH_ROUNDS = 10;
--- section10-jwt/src/users/users.controller.ts
+++ section10-jwt/src/users/users.controller.ts
@@ -12,7 +12,11 @@ export class UsersController {
     @Body('email') email: string,
     @Body('password') password: string,
   ) {
-    return this.usersService.createUser(nickname, email, password);
+    return this.usersService.createUser({
+      nickname,
+      email,
+      password,
+    });
   }
 
   @Get()
