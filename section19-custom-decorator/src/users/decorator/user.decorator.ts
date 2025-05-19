84be3307913eb89d5f4bc54a51d7b87d47cb3dcd
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-05-19 16:42:33 +0900

    커스텀 데코레이터의 data 파라미터 사용하기

--- section19-custom-decorator/src/users/decorator/user.decorator.ts
+++ section19-custom-decorator/src/users/decorator/user.decorator.ts
@@ -4,16 +4,23 @@ import {
   InternalServerErrorException,
   UnauthorizedException,
 } from '@nestjs/common';
+import { UsersModule } from '../users.module';
 
-export const User = createParamDecorator((data, context: ExecutionContext) => {
-  const req = context.switchToHttp().getRequest();
+export const User = createParamDecorator(
+  (data: keyof UsersModule | undefined, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest();
 
-  const user = req.user;
+    const user = req.user as UsersModule;
 
-  if (!user) {
-    throw new InternalServerErrorException(
-      'Request에 user 프로퍼티가 존재하지 않습니다.',
-    );
-  }
-  return user;
-});
+    if (!user) {
+      throw new InternalServerErrorException(
+        'Request에 user 프로퍼티가 존재하지 않습니다.',
+      );
+    }
+
+    if (data) {
+      return user[data];
+    }
+    return user;
+  },
+);
