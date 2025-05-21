ff70792fd84020d7bec148b0bc9967473a13ca7e
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-05-21 11:26:17 +0900

    ImplicitConversion 적용해보기

--- section23-pagination/src/main.ts
+++ section23-pagination/src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
       // query에 실제로 값을 넣은 적이 없다면 원래 그대로 반환을 해줘야 하는데,
       // 이를 설정해주면 값을 넣지 않아도 DTO에 넣은 값을 넣은 채로 해줌
       transform: true,
+      transformOptions: {
+        // class-validator를 기준으로 @IsNumber()로 되어 있으면,
+        // 자동으로 number로 변환을 해줌
+        enableImplicitConversion: true,
+      },
     }),
   );
   await app.listen(3000);
