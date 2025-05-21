a94049c0e5775124e01c15d00e78cbf8e4e81aac
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-05-21 10:58:07 +0900

    pagination 초기설정

---
+++ section23-pagination/src/posts/posts.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostsController } from './posts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModel } from './entities/posts.entity';
+import { AuthService } from '../auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [
+    // 해당 모델의 레포지토리를 주입할 떄 forFeature를 씀
+    TypeOrmModule.forFeature([PostsModel]),
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [PostsController],
+  providers: [PostsService],
+})
+export class PostsModule {}
