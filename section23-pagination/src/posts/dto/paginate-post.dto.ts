2a706dbf6181cf7a6d106d313922da1f65683a0c
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-05-22 10:55:38 +0900

    BasePaginationDto 생성하기

--- section23-pagination/src/posts/dto/paginate-post.dto.ts
+++ section23-pagination/src/posts/dto/paginate-post.dto.ts
@@ -1,28 +1,4 @@
-import { IsIn, IsNumber, IsOptional } from 'class-validator';
+import { BasePaginateDto } from '../../common/dto/base-paginate.dto';
 
-export class PaginatePostDto {
-  @IsNumber()
-  @IsOptional()
-  page?: number;
-
-  @IsNumber()
-  @IsOptional()
-  where__id_less_than?: number;
-
-  // 이전 마지막 데이터의 ID
-  // 이 프로퍼티에 입력된 ID 보다 높은 ID 부터 값을 가져오기
-  @IsNumber()
-  @IsOptional()
-  where__id_more_than?: number;
-
-  // 정렬
-  // createdAt -> 생성된 시간의 내림차/오름차 순으로 정렬
-  @IsIn(['ASC', 'DESC'])
-  @IsOptional()
-  order__createdAt: 'ASC' | 'DESC' = 'ASC';
-
-  // 몇 개의 데이터를 응답으로 받을지
-  @IsNumber()
-  @IsOptional()
-  take: number = 20;
-}
+// BsePaginationDto를 상속했기에 추가하고 싶은 속성이 있으면 그냥 추가만 해주면 됨
+export class PaginatePostDto extends BasePaginateDto {}
