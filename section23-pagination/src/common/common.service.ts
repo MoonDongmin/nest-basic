f05f7d1b27c386a49df2f66813323ed8a09e9bf2
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-05-22 13:01:41 +0900

    BasePaginationDto 리팩터링 & paginate()함수 선언하기

--- section23-pagination/src/common/common.service.ts
+++ section23-pagination/src/common/common.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { BasePaginateDto } from './dto/base-paginate.dto';
+import { FindManyOptions, Repository } from 'typeorm';
+import { BaseModel } from './entity/base.entity';
 
 @Injectable()
-export class CommonService {}
+export class CommonService {
+  paginate<T extends BaseModel>(
+    dto: BasePaginateDto,
+    respository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T>,
+    path: string,
+  ) {}
+}
