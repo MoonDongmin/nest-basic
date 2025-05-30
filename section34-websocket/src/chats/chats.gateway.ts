8501c5810d13510f5d78f68b09532541ebf602fc
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-05-30 16:48:00 +0900

    Room 활용하기

--- section34-websocket/src/chats/chats.gateway.ts
+++ section34-websocket/src/chats/chats.gateway.ts
@@ -1,4 +1,5 @@
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   SubscribeMessage,
@@ -21,10 +22,29 @@ export class ChatsGateway implements OnGatewayConnection {
     console.log(`on connect called : ${socket.id}`);
   }
 
+  // 채팅방에 들어가게
+  @SubscribeMessage('enter_chat')
+  enterChat(
+    // 방의 chat ID들을 리스트로 받음
+    @MessageBody() data: number[],
+    // 이 소켓에 연결된 소켓을 들고옴
+    @ConnectedSocket() socket: Socket,
+  ) {
+    for (const chatId of data) {
+      // socket.join()
+      socket.join(chatId.toString());
+    }
+  }
+
   // socket.on('send_message', (message)=>{console.log(message)});
   @SubscribeMessage('send_message')
-  sendMessage(@MessageBody() message: string) {
+  sendMessage(
+    @MessageBody() message: { message: string; chatId: number },
+    @ConnectedSocket() socket: Socket,
+  ) {
     // 연결된 모든 소켓에게 receive_message 이벤트를 받는 곳에게 다음 메시지를 보내라라는 의미
-    this.server.emit('receive_message', 'hello from server');
+    this.server
+      .in(message.chatId.toString())
+      .emit('receive_message', message.message);
   }
 }
