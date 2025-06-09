26900294fad76f9891bbfce176df0eb4373a04e6
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-06-09 15:52:26 +0900

    Comment 생성 API 작업하기

--- section36-module-nesting/src/posts/comments/comments.service.ts
+++ section36-module-nesting/src/posts/comments/comments.service.ts
@@ -4,6 +4,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CommentsModel } from './entity/comments.entity';
 import { Repository } from 'typeorm';
 import { CommonService } from '../../common/common.service';
+import { CreateCommentsDto } from './dto/create-comments.dto';
+import { UsersModel } from '../../users/entity/users.entity';
+import { DEFAULT_COMMENT_FIND_OPTIONS } from './const/default-comment-find-options.const';
 
 @Injectable()
 export class CommentsService {
@@ -23,6 +26,7 @@ export class CommentsService {
             id: postId,
           },
         },
+        ...DEFAULT_COMMENT_FIND_OPTIONS,
       },
       `posts/${postId}/comments`,
     );
@@ -30,6 +34,7 @@ export class CommentsService {
 
   async getCommentById(id: number) {
     const comment = await this.commentsRepository.findOne({
+      ...DEFAULT_COMMENT_FIND_OPTIONS,
       where: {
         id,
       },
@@ -40,5 +45,19 @@ export class CommentsService {
     }
 
     return comment;
+  }
+
+  async createComment(
+    dto: CreateCommentsDto,
+    postId: number,
+    author: UsersModel,
+  ) {
+    return this.commentsRepository.save({
+      ...dto,
+      post: {
+        id: postId,
+      },
+      author,
+    });
   }
 }
